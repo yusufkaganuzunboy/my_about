431f88c72e0dd1be2e5e5fd75667ba1ae96df8c3
yusufkaganuzunboy <yusufkagann0@gmail.com>
2025-08-03 21:32:07 +0300

    Add files via upload
    INFORMATION ABOUT ME

---
+++ Yusuf.java
@@ -0,0 +1,7 @@
+public class Yusuf{
+    String name = "Yusuf Kağan";
+    String surname = "UZUNBOY";
+    int age = 18;
+    String job = "Software Engineering Student";
+    String knownLanguages = "Java";
+}
